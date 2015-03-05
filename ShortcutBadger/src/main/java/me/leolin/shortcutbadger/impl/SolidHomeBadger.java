2c155379a503ce6e741efffb3c2055f034887a61
Majeur <majeur.android@gmail.com>
2015-03-05 19:16:44 +0100

    Added support for Solid launcher

---
+++ ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/SolidHomeBadger.java
@@ -0,0 +1,32 @@
+package me.leolin.shortcutbadger.impl;
+
+import android.content.Context;
+import android.content.Intent;
+
+import me.leolin.shortcutbadger.ShortcutBadgeException;
+import me.leolin.shortcutbadger.ShortcutBadger;
+
+/**
+ * @author MajeurAndroid
+ */
+public class SolidHomeBadger extends ShortcutBadger {
+
+    private static final String INTENT_UPDATE_COUNTER = "com.majeur.launcher.intent.action.UPDATE_BADGE";
+    private static final String PACKAGENAME = "com.majeur.launcher.intent.extra.BADGE_PACKAGE";
+    private static final String COUNT = "com.majeur.launcher.intent.extra.BADGE_COUNT";
+    private static final String CLASS = "com.majeur.launcher.intent.extra.BADGE_CLASS";
+
+    public ApexHomeBadger(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void executeBadge(int badgeCount) throws ShortcutBadgeException {
+
+        Intent intent = new Intent(INTENT_UPDATE_COUNTER);
+        intent.putExtra(PACKAGENAME, getContextPackageName());
+        intent.putExtra(COUNT, badgeCount);
+        intent.putExtra(CLASS, getEntryActivityName());
+        mContext.sendBroadcast(intent);
+    }
+}
