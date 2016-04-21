769e9458f1ca75e0369fcfbb58e8359d01e5c649
Olivier Payen <olivier.payen@gmail.com>
2016-04-21 13:31:40 +0200

    Restore the imports like on the original repo

--- ShortcutBadger/src/main/java/me/leolin/shortcutbadger/ShortcutBadger.java
+++ ShortcutBadger/src/main/java/me/leolin/shortcutbadger/ShortcutBadger.java
@@ -7,19 +7,11 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Build;
 import android.util.Log;
+import me.leolin.shortcutbadger.impl.*;
 
+import java.lang.reflect.Constructor;
 import java.util.LinkedList;
 import java.util.List;
-
-import me.leolin.shortcutbadger.impl.AdwHomeBadger;
-import me.leolin.shortcutbadger.impl.ApexHomeBadger;
-import me.leolin.shortcutbadger.impl.AsusHomeLauncher;
-import me.leolin.shortcutbadger.impl.DefaultBadger;
-import me.leolin.shortcutbadger.impl.NewHtcHomeBadger;
-import me.leolin.shortcutbadger.impl.NovaHomeBadger;
-import me.leolin.shortcutbadger.impl.SolidHomeBadger;
-import me.leolin.shortcutbadger.impl.SonyHomeBadger;
-import me.leolin.shortcutbadger.impl.XiaomiHomeBadger;
 
 
 /**
