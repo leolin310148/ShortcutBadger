6d4f3f003f13db5b18221164f386acd93a09c09f
Tom Kliethermes <kliethermes.tom@gmail.com>
2017-10-24 07:37:46 -0500

    fix accidental quote

--- ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/DefaultBadger.java
+++ ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/DefaultBadger.java
@@ -16,7 +16,7 @@ import me.leolin.shortcutbadger.util.BroadcastHelper;
  * @author leolin
  */
 public class DefaultBadger implements Badger {
-    private static final String INTENT_ACTION = "android.intent.action.BADGE_COUNT_UPDATE"";
+    private static final String INTENT_ACTION = "android.intent.action.BADGE_COUNT_UPDATE";
     private static final String INTENT_EXTRA_BADGE_COUNT = "badge_count";
     private static final String INTENT_EXTRA_PACKAGENAME = "badge_count_package_name";
     private static final String INTENT_EXTRA_ACTIVITY_NAME = "badge_count_class_name";
