25f567c98de5c85e1e5e7cac7d57d290f8017e13
Nilesh Deokar <nieldeokar@gmail.com>
2018-08-02 16:07:39 +0200

    Fixed NPE
    Issue #300 fixed

--- SampleApp/src/main/java/me/leolin/shortcutbadger/example/BadgeIntentService.java
+++ SampleApp/src/main/java/me/leolin/shortcutbadger/example/BadgeIntentService.java
@@ -24,9 +24,14 @@ public class BadgeIntentService extends IntentService {
     private NotificationManager mNotificationManager;
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+    
+    @Override
     public void onStart(Intent intent, int startId) {
         super.onStart(intent, startId);
-        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     @Override
